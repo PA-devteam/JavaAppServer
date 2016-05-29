169ea4a17e0a12fb106e03c786c681c2affa4e20
sfricker <fricker.sebastien@orange.fr>
2016-05-29 17:15:00 +0200

    Adding an EquationsManager class to manage equations operation (CRUD), Adding 2 interfaces for standard CRUD Op & Initialisable class Updating Equation with getter/setter

--- src/entities/Equation.java
+++ src/entities/Equation.java
@@ -29,4 +29,70 @@ public class Equation {
    /** @pdRoleInfo migr=no name=Element assc=equationElement mult=1..* type=Composition */
    public Element[] equationElement;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getIsValid() {
+        return isValid;
+    }
+
+    public void setIsValid(Boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    public User getValidatedBy() {
+        return validatedBy;
+    }
+
+    public void setValidatedBy(User validatedBy) {
+        this.validatedBy = validatedBy;
+    }
+
+    public Date getValidationDate() {
+        return validationDate;
+    }
+
+    public void setValidationDate(Date validationDate) {
+        this.validationDate = validationDate;
+    }
+
+    public Rule[] getEquationRules() {
+        return equationRules;
+    }
+
+    public void setEquationRules(Rule[] equationRules) {
+        this.equationRules = equationRules;
+    }
+
+    public Element[] getEquationElement() {
+        return equationElement;
+    }
+
+    public void setEquationElement(Element[] equationElement) {
+        this.equationElement = equationElement;
+    }
+
+   
+   
 }
